cd04819d12ae157810a264b23dd70fb28530947a
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-08-09 16:41:16 +0530

    Revise Data-Types

--- Learning/index.js
+++ Learning/index.js
@@ -9,7 +9,7 @@
         // console.error("this is error");
 
         //3. Javascript Variables
-        //What is Variable? - It is a Container to store data values
+        //What is Variable? - It is a Container to store data values - var, let, const are the keywords to declare variables in Javascript
         var number1=456
         var number2 = 799
         console.log(number1 + number2);
@@ -29,3 +29,50 @@
         let no1=7
         let no2=3
         console.log(no1+no2);
+
+        let n1="Software"
+        let n2=" Developer"
+        console.log(n1 + n2);
+
+        //4. Data Types in Javascript
+        // Numbers
+        var num3=78;
+        var num4=45.73;
+        
+        // String
+        var str1="My name is Gauri";
+        var str2=" I am a Software Developer";
+
+        // Objects
+        var marks={Gauri:91.80, Sakshi:85.60, Priya:80.72};
+
+        // Boolean
+        var a=true;
+        var b=false;
+
+        console.log(num3+num4);
+        console.log(str1+str2);
+        console.log(marks);
+        console.log(a);
+        console.log(b);
+
+        // Undefined
+        //var und="undefined";
+        var und;
+        console.log(und);
+
+        // Null 
+        var n = null;
+        console.log(n);
+
+       /* There are two types of datatypes in Javascript
+        1. Primitive data types: number, string, boolean, undefined, null, symbol
+        2. Reference data types: Arrays and Objects */
+
+        // Array
+        var arr=[23, 7, 79,"Gauri", 24, 23];
+        console.log(arr);
+
+        var arr1=[12, 65, "Peru", "Lotus", 76, "India", "Software-Developer"];
+        
+
