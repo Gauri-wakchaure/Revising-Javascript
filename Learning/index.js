1181968fb39c344128f14b779d70a04b5b3a03ca
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-08-09 15:25:49 +0530

    Revise Variables

---
+++ Learning/index.js
@@ -0,0 +1,31 @@
+       //1. Ways to print in Javascript
+        // console.log("Hello Gauri");
+        // alert("me")
+        // document.write("hello World"); 
+
+        //2. Javascript console API
+        // console.log("Hello World");
+        // console.warn("this is warning");
+        // console.error("this is error");
+
+        //3. Javascript Variables
+        //What is Variable? - It is a Container to store data values
+        var number1=456
+        var number2 = 799
+        console.log(number1 + number2);
+
+        var name1 = "Gauri"
+        var name2 = " Wakchaure"
+        console.log(name1 + name2);
+
+        var name3="Hello,"
+        var name4= " How are you?"
+        console.log(name3 + name4);
+
+        var num1= 65
+        var num2= 76
+        console.log(num1*num2);
+
+        let no1=7
+        let no2=3
+        console.log(no1+no2);
