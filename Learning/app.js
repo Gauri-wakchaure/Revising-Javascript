23ae6ed792d6b2749999182d0b462d96bc5189e7
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-08-21 11:13:23 +0530

    Revise Conditional Statements

---
+++ Learning/app.js
@@ -0,0 +1,79 @@
+console.log("Hello Gauri!");
+console.log("Gauri Wakchaure");
+let a=90;
+let b=23;
+sum=a+b;
+console.log("Sum of a and b is: "+sum);
+
+//Arithmetic Operators
+let x=10;
+let y=5;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+//Unary Operators
+let num1=10;
+console.log(num1++);
+console.log(++num1);
+
+//Assignment Operators
+let c=11;
+let d=c;
+console.log(d);
+
+//if else statement
+let age=18;
+if(age>=18)
+{
+    console.log("Eligible for vote");
+}
+else{
+     console.log("Not Eligible for vote");
+}
+
+//if else if statements
+let color="Green";
+if(color==="red")
+{
+   console.log("Traffic light is red You have to Stop");
+}
+else if(color==="Yellow")
+{
+    console.log("Traffic light is Yellow You have go slow");
+}
+else if(color==="Green")
+{
+    console.log("Traffic light is Green You can Go");
+}
+else{
+    console.log("False Condition");
+}
+
+//Example
+let size='L';
+if(size=='XL')
+{
+    console.log("Price is 250");
+}
+else if(size=='L'){
+    console.log("Price is 200");
+}
+else if(size=='M'){
+    console.log("Price is 100");
+}
+else if(size=='S')(
+    console.log("Price is 50")
+)
+
+//Logical Operator Example
+let str1="app";
+if((str1[0]==='a') && (str1.length>3))
+{
+    console.log("It is a Good String");
+}
+else{
+    console.log("It is not Good string");
+}
