723bc5de765cb56e8d418d1adacf91d2a9fe7fae
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-01 12:47:21 +0530

    Learn Object Literals and Math Methods

--- Learning/app.js
+++ Learning/app.js
@@ -253,8 +253,38 @@ let a3=["App","Run","Code"];
 a3.includes("Gauri");   //false
 a3.includes("Code");    //true
 
+//JS Part-5 
+const Post={
+    username: "@GauriWakchaure",
+    content:" #Hello EveryOne",
+    like: 150,
+    reposts: 50,
+    tags:["Gau", "Hello"]
+};
 
+//Generate randome numbers
+let step1=Math.floor(Math.random()*100);
 
+//Number Guessing Game
+const max=prompt("Enter Max Number");
+const random=Math.floor(Math.random()*max)+1;
+
+let guess=prompt("Guess the number");
+while(true){
+    if(guess=="quit"){
+        console.log("User Quit");
+        break;
+    }
+    if(guess == random){
+        console.log("you are right! Congrats!!! Your Random number was "+random);
+        break;
+    }else if(guess < random){
+        guess=prompt("hint: your guess was too small. Please try again")
+    }
+    else {
+        guess = prompt("hint: Your guess was too large, Please try again");
+    }
+}
 
 
 
